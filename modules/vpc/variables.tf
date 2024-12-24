4fe066a01abfa3d118967bdcabd62385c6f32647
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-24 15:12:15 +0530

    feat: create vpc main resources

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -8,3 +8,24 @@ variable "vpc_name" {
   type        = string
   description = "name for main vpc"
 }
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "availability zone list"
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "cidr values for public subnet"
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "cidr values for ptivate subnet"
+}
+
+variable "enable_NAT_gateway" {
+  type        = bool
+  description = "NAT gateway available or not"
+  default     = false
+}
