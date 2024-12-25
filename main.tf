c564ffdfe0e17d402c6a8b071bddb9074c8d3ab6
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-25 09:19:22 +0530

    call vpc module

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ provider "aws" {
   region = var.region
 }
 
+module "main_vpc" {
+  source               = "./modules/vpc"
+  vpc_name             = "Backend-VPC"
+  cidr_block           = "11.10.0.0/16"
+  availability_zones   = ["eu-north-1a", "eu-north-1b"]
+  private_subnet_cidrs = ["11.10.3.0/24", "11.10.4.0/24"]
+}
