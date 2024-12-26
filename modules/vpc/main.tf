6276ccea1c827cddd2f6948c4388ba1dd11d78a6
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-26 18:28:01 +0530

    fix : bug 2.0

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -69,7 +69,7 @@ resource "aws_eip" "elastic_IP_address" {
 resource "aws_nat_gateway" "nat_gateway" {
   count         = length(var.private_subnet_cidrs)
   allocation_id = aws_eip.elastic_IP_address[count.index].id
-  subnet_id     = element(aws_subnet.public_subnet[*].id, 0, count)
+  subnet_id     = element(aws_subnet.public_subnet[*].id, count.index)
 }
 
 resource "aws_route" "private_route" {
