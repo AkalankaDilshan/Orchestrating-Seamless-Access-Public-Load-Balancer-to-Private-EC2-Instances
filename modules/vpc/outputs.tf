b4904e5bbb0c30a328869d7cf16e659566998e18
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-26 17:54:35 +0530

    feat:add missing output values :ab:

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  value = aws_vpc.main_vpc.id
+}
+output "vpc_name" {
+  value = var.vpc_name
+}
+output "vpc_cidr" {
+  value = aws_vpc.main_vpc.cidr_block
+}
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet[*].id
+
+}
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet[*].id
+}
+output "nat_gateway_id" {
+  value = aws_nat_gateway.nat_gateway[*].id
+}
