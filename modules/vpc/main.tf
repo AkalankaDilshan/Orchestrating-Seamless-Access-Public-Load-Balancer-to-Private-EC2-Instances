31dd943ff367c76909042a1be3cc293dbbf6e7d5
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-26 20:51:50 +0530

    try to fix : 1.0

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -73,12 +73,17 @@ resource "aws_nat_gateway" "nat_gateway" {
 }
 
 resource "aws_route" "private_route" {
-  count                  = length(var.private_subnet_cidrs)
+  # count                  = length(var.private_subnet_cidrs)
   route_table_id         = aws_route_table.private_rt.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat_gateway[count.index].id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway[0].id
 }
-
+resource "aws_route" "private_route" {
+  # count                  = length(var.private_subnet_cidrs)
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gateway[1].id
+}
 resource "aws_route_table_association" "private_RT_association" {
   count          = length(aws_subnet.private_subnet)
   subnet_id      = aws_subnet.private_subnet[count.index].id
