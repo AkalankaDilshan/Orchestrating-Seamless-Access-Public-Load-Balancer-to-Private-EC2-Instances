6f4984a08e9dcca2fe3c1cefb3f43adc233632fd
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-26 22:37:29 +0530

    feat : call ec2 and key pair :adhesive_bandage:

--- main.tf
+++ main.tf
@@ -17,3 +17,31 @@ module "ec2_security_group" {
   vpc_id          = module.main_vpc.vpc_id
   nat_gateway_eip = flatten([module.main_vpc.nat_gateway_eip])
 }
+
+module "key_pair" {
+  source = "./modules/key-pair"
+}
+
+module "backend_instance_blue" {
+  source                = "./modules/ec2"
+  instance_name         = "blue_instance"
+  instance_type         = "t3.micro"
+  subnet_id             = module.main_vpc.private_subnet_id[0]
+  vpc_security_group_id = module.ec2_security_group.ec2_sg_id[0]
+  ebs_volume_size       = 8
+  ebs_volume_type       = "gp2"
+  key_pair_name         = module.key_pair.key_pair_name
+  depends_on            = [module.key_pair, module.ec2_security_group]
+}
+
+module "backend_instance_green" {
+  source                = "./modules/ec2"
+  instance_name         = "green_instance"
+  instance_type         = "t3.micro"
+  subnet_id             = module.main_vpc.private_subnet_id[1]
+  vpc_security_group_id = module.ec2_security_group.ec2_sg_id[1]
+  ebs_volume_size       = 8
+  ebs_volume_type       = "gp2"
+  key_pair_name         = module.key_pair.key_pair_name
+  depends_on            = [module.key_pair, module.ec2_security_group]
+}
