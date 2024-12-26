f046e98f17728bdf9d7954e4f67f1c844554b4fc
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-26 15:09:23 +0530

    feat:create initial sg for ec2

---
+++ modules/security_group_ec2/variables.tf
@@ -0,0 +1,9 @@
+variable "sg_name" {
+  type        = string
+  description = "name for the security group for ec2"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "ID for main VPC"
+}
