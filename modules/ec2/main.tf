605128b7b7107e6a3370d741ab9dcf3386a25663
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-26 22:44:12 +0530

    fix : missing args error

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "server_instance" {
   instance_type           = var.instance_type
   subnet_id               = var.subnet_id
   vpc_security_group_ids  = [var.vpc_security_group_id]
-  user_data               = templatefile("init-script.sh")
+  user_data               = templatefile("init-script.sh", {})
   disable_api_termination = false
   ebs_optimized           = false
   root_block_device {
