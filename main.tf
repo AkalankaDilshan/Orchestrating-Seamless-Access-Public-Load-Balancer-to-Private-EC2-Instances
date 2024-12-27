bafde914bdc3128708af68520b06f86d0375c86e
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-27 15:44:06 +0530

    fix :avoid nat gateway

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ module "application_load_balancer" {
   # alb_subnet_id     = module.main_vpc.public_subnet_id[0]
   # public_subnet_ids = module.main_vpc.public_subnet_id[0]
   alb_subnet_id     = flatten([module.main_vpc.public_subnet_id])
-  target_ids        = flatten([module.main_vpc.nat_gateway_allocation_ids])
+  target_ids        = [module.backend_instance_blue.instance_private_ip, module.backend_instance_green.instance_private_ip]
   security_group_id = module.application_load_balancer_sg.alb_security_group_id
 
 }
