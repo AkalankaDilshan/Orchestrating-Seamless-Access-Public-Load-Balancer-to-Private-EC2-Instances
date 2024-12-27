8801d47ad5735d16f9a986631cb2194659f1780a
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-27 16:49:03 +0530

    feat: temporary disable alb module 2.0 🔢

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "ec2_key_pair" {
   value = module.key_pair.key_pair_name
 }
 
-output "alb_dns_name" {
-  value = module.application_load_balancer.alb_dns_name
-}
+# output "alb_dns_name" {
+#   value = module.application_load_balancer.alb_dns_name
+# }
