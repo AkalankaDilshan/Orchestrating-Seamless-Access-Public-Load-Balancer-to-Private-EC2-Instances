8fe722ecfe85dddcd8d040cc66f35e9836e7443e
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-27 21:20:49 +0530

    feat: change sg for communicate with lb

--- modules/security_group_ec2/main.tf
+++ modules/security_group_ec2/main.tf
@@ -7,6 +7,28 @@ resource "aws_security_group" "server_sg" {
   }
 }
 
+#connecte with load balancer
+resource "aws_security_group_rule" "allow_ssh" {
+  type              = "ingress"
+  description       = "SSH ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.server_sg.id
+}
+
+resource "aws_security_group_rule" "allow_http" {
+  type              = "ingress"
+  description       = "HTTP ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.server_sg.id
+}
+
+#communicate for NAT gateway
 resource "aws_security_group_rule" "allow_ssh" {
   type              = "ingress"
   description       = ""
